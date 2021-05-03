ea83f55e78024ace9592528e069a0e0e65de2010
unknown <salimajah01@gmail.com>
2021-05-03 21:12:24 +0700

    finishing

--- WindowsApplication3/LaporanDataPeminjam.vb
+++ WindowsApplication3/LaporanDataPeminjam.vb
@@ -5,11 +5,10 @@
         Dim no As Integer
         Me.dg_lap_peminjam.Rows.Clear()
         buka()
-        rek.Open("Select * from tbl_buku where judul like '%" & txt_pencarian.Text & "%' order by judul", kon, 3, 2)
+        rek.Open("Select * from tbl_pinjam_buku where nama_pinjam like '%" & txt_pencarian.Text & "%' order by nama_pinjam", kon, 3, 2)
         no = 1
         Do While Not rek.EOF
-
-            Me.dg_lap_peminjam.Rows.Add(no, rek.Fields("judul").Value, rek.Fields("pengarang").Value, rek.Fields("penerbit").Value, rek.Fields("stok").Value, rek.Fields("lokasi").Value)
+            Me.dg_lap_peminjam.Rows.Add(no, rek.Fields("nama_pinjam").Value, rek.Fields("nama_buku").Value, rek.Fields("tgl_pinjam").Value, rek.Fields("tgl_kembali").Value)
             rek.MoveNext()
             no = no + 1
         Loop
@@ -17,19 +16,19 @@
     End Sub
 
     Private Sub LaporanDataPeminjam_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim judul() As String = {"No", "Judul Buku", "Pengarang", "Penerbit", "Stok", "Lokasi"}
-        Dim lebar() As String = {50, 200, 200, 200, 50, 0}
+        Dim judul() As String = {"No", "Nama Peminjam", "Judul Buku", "Tgl. Pinjam", "Tgl. Kembali"}
+        Dim lebar() As String = {30, 170, 200, 150, 150}
         Dim i As Integer
         dg_lap_peminjam.RowHeadersVisible = False
-        dg_lap_peminjam.ColumnCount = 6
+        dg_lap_peminjam.ColumnCount = 5
         dg_lap_peminjam.RowCount = 1
         dg_lap_peminjam.SelectionMode = DataGridViewSelectionMode.CellSelect
-        dg_lap_peminjam.ColumnHeadersDefaultCellStyle.Font = New Font("Arial", 13)
+        dg_lap_peminjam.ColumnHeadersDefaultCellStyle.Font = New Font("Arial", 11)
 
         For i = 0 To dg_lap_peminjam.ColumnCount - 1
             dg_lap_peminjam.Columns(i).HeaderText = judul(i)
             dg_lap_peminjam.Columns(i).Width = lebar(i)
-            dg_lap_peminjam.Columns(i).DefaultCellStyle.Font = New Font("Arial", 11)
+            dg_lap_peminjam.Columns(i).DefaultCellStyle.Font = New Font("Arial", 9)
             dg_lap_peminjam.Columns(i).DefaultCellStyle.BackColor = Color.AliceBlue
         Next
 
@@ -37,17 +36,14 @@
         Dim no As Integer
         Me.dg_lap_peminjam.Rows.Clear()
         buka()
-        rek.Open("Select * from tbl_buku where judul like '%" & txt_pencarian.Text & "%' order by judul", kon, 3, 2)
+        rek.Open("Select * from tbl_pinjam_buku where nama_pinjam like '%" & txt_pencarian.Text & "%' order by nama_pinjam", kon, 3, 2)
         no = 1
         Do While Not rek.EOF
-            Me.dg_lap_peminjam.Rows.Add(no, rek.Fields("judul").Value, rek.Fields("pengarang").Value, rek.Fields("penerbit").Value, rek.Fields("stok").Value, rek.Fields("lokasi").Value)
+            Me.dg_lap_peminjam.Rows.Add(no, rek.Fields("nama_pinjam").Value, rek.Fields("nama_buku").Value, rek.Fields("tgl_pinjam").Value, rek.Fields("tgl_kembali").Value)
             rek.MoveNext()
             no = no + 1
         Loop
         tutup()
     End Sub
-    Private Sub dg_lap_peminjam_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dg_lap_peminjam.CellContentClick
-        RakBuku.lbl_rak_buku.Text = dg_lap_peminjam.Rows(e.RowIndex).Cells(5).Value
-        RakBuku.ShowDialog()
-    End Sub
+
 End Class
